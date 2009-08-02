48632d8e40fe75b8e06ae03aa97b778dca9a6ca6
Boris Kolpackov <boris@codesynthesis.com>
2009-08-02 17:06:08 +0200

    Add compiler driver stub

---
+++ cli/cli.cxx
@@ -0,0 +1,13 @@
+// file      : cli/cli.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : Copyright (c) 2009 Code Synthesis Tools CC
+// license   : TBD
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  cerr << "CLI compiler driver" << endl;
+}
